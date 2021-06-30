7374c09c6cb73c18afed230b971a357df8fa64af
raoufbecher <raouf-94@live.fr>
2021-06-30 14:46:48 +0100

    modifyng

--- server.js
+++ server.js
@@ -3,19 +3,25 @@ const app =express()
 const port = 5000;
 const d = new Date( Date.now());
 
+
+
 function condition(req,res,next) {
     const day =d.getDay();
     const hour =d.getHours();
     if ((day >=2 && day<=6 )  && (hour >=9 && hour<=10)  === false ) {
     console.log ("close");
-    
-    res.sendFile(__dirname + '/public/close/close.html');  
+    res.redirect('/close.html')
+    //res.sendFile(__dirname + '/public/close/close.html');  
 }  
 
 else {
 next();
 }
-}  
+} 
+/* app.get('/close.html',(req,res)=>{
+    res.sendFile(`${__dirname}/public/close/close.html`)
+})  */
+app.use(express.static('public/close'));
 app.use(condition);
 app.use(express.static('public'));
 app.listen(port,(err)=> {
